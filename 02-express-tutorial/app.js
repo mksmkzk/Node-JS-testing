fcd8c5eb5093fc6e08851254421d8e87e2bc0414
mksmkzk <maksakozak@gmail.com>
2022-08-16 22:11:37 -0700

    further progress

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,52 +1,19 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+const express = require('express');
+const path = require('path');
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  console.log(url)
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
-  }
-  // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
-  }
-  // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
-})
+// Setup static and middleware
+app.use(express.static('./public'));
 
-server.listen(5000)
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,  './navbar-app/index.html'));
+// });
+
+app.get('*', (req, res) => {
+    res.status(404).send('Resource Not Found');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
